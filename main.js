91f4f1fb060db9ca496390eb0d46b293195f8dff
DimitriHinds <81481893+DimitriHinds@users.noreply.github.com>
2021-12-11 12:26:42 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -5,9 +5,31 @@ function setup()
 {
     canvas = createCanvas(300, 300);
     canvas.center();
+    v = createCapture(VIDEO);
+    v.size(300, 300);
+    v.hide();
+
+    poseNet = ml5.poseNet(v, modelLoaded);
+    poseNet.on('pose', gotPoses);
+}
+
+function modelLoaded()
+{
+    console.log('PoseNet Is Initialized');
+}
+
+function gotPoses(results)
+{
+    if(results.length > 0)
+    {
+        console.log(results)
+        console.log("nose x = " + results[0].pose.nose.x);
+        console.log("nose y = " + results[0].pose.nose.y);
+    }
 }
 
 function draw(){
+image(v, 0, 0, 300, 300);
 }
 
 function take_snapshot(){
